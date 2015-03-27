6a6e9b6ab6552c7bd59c6a2405972bbd40236019
esteban launay <etochy@gmail.com>
2015-03-27 07:14:28 +0100

    plpo

--- www/js/index.js
+++ www/js/index.js
@@ -36,71 +36,7 @@ function year() {
 	}	      
 }
 
-function getDetailsFromEleve(Num){
-	loadXMLFile();
-	document.write("<table  class='tabcenter'><tr><th>DETAILS</th></tr>");
-	var x=xmlDoc.getElementsByTagName("BEFORE");
-	for (i=0;i<x.length;i++){
-		if(x[i].parentNode.getAttribute('Num')==Num){
-			document.write("<tr><td>");
-			document.write("Date de naissance : "+x[i].getAttribute('BirthDate')+" ");
-			document.write("Bac : "+x[i].getAttribute('Bac')+" ");
-			document.write("</td></tr>");
-	}
-  }
-	document.write("</table>");
-}
-function getEleveFromPromotion(Dep, Diplome, Year){
-	loadXMLFile();
-	document.write("<table  class='tabcenter'><tr><th>ELEVES</th></tr>");
-	var x=xmlDoc.getElementsByTagName("ETU");
-	for (i=0;i<x.length;i++){
-		if(x[i].parentNode.parentNode.parentNode.getAttribute('Name')==Dep && x[i].parentNode.parentNode.getAttribute('Name')==Diplome && x[i].parentNode.getAttribute('Id')==Year){
-			document.write("<tr><td>");
-			document.write("<a href= "+"etu.html"+" onclick= "+"actualisation("+x[i].getAttribute('LastName')+"); >"+x[i].getAttribute('LastName')+"</a>");
-			document.write("</td></tr>");
-	}
-  }
-	document.write("</table>");
-}
 
-function getYearFromDiplome(Dep, Diplome){
-	loadXMLFile();
-	document.write("<table class='tabcenter'><tr><th>PROMOTION</th></tr>");
-	var x=xmlDoc.getElementsByTagName("YEAR");
-	for (i=0;i<x.length;i++){
-		if(x[i].parentNode.parentNode.getAttribute('Name')==Dep && x[i].parentNode.getAttribute('Name')==Diplome ){
-			document.write("<tr><td>");
-			document.write(x[i].getAttribute('Id'));
-			document.write("</td></tr>");
-	}
-  }
-	document.write("</table>");
-}
-function getDiplomeFromDep(Dep){
-	loadXMLFile();
-	document.write("<table  class='tabcenter'><tr><th>DIPLOME</th></tr>");
-	var x=xmlDoc.getElementsByTagName("DIPLOME");
-	for (i=0;i<x.length;i++){
-		if(x[i].parentNode.getAttribute('Name')==Dep){
-			document.write("<tr><td>");
-			document.write(x[i].getAttribute('Name'));
-			document.write("</td></tr>");
-	}
-  }
-	document.write("</table>");
-}
-function chooseDep(){
-	loadXMLFile();
-	document.write("<table  class='tabcenter'><tr><th>DEPARTEMENT</th></tr>");
-	var x=xmlDoc.getElementsByTagName("DEP");
-	for (i=0;i<x.length;i++){
-		document.write("<tr><td>");
-		document.write(x[i].getAttribute('Name'));
-		document.write("</td></tr>");
-	}
-	document.write("</table>");
-}
 function loadXMLFile(){
 	if (window.XMLHttpRequest)
 	{// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -122,12 +58,11 @@ function readStudent(form2){
 	}
 	document.form2.output.value=num; 
 }
-
 function refresh(form1) {
-        name = document.form1.reNom.value;
+        dep = document.form1.reDep.value;
         annee = document.form0.year.value;
-
-        var requete = name+"/"+annee;
+		dip = document.form1.reDip.value;
+        var requete = dep+"/"+annee+"/"+dip;
         window.location.href = "index.html?" + requete ;
 
 }
@@ -143,30 +78,74 @@ function actualisation(num){
 function getAll(){
     var requete = window.location.search.substring(1).split("/") ;
 	if (requete != "") {
-		name = requete[0];
+		dep = requete[0];
 		annee = requete[1];
+		dip = requete[2];
 	}
-	
-	if (name == "" && annee == "tous"){
+
+	if (dep == "" && annee == "tous" && dip == ""){
 		getAllEmpty();
 	}
-	else if (name == "" && annee != "tous"){
+	else if (dep == "" && annee != "tous" && dip == ""){
 		getAllByAnnee();
 	}
-	else if (name != "" && annee == "tous"){
-		getAllByName();
+	else if (dep != "" && annee == "tous" && dip == ""){
+		getAllByDep();
+	}
+	else if (dep == "" && annee == "tous" && dip != ""){
+		getAllByDip();
+	}
+	else if (dep != "" && annee != "tous" && dip == ""){
+		getAllByDepAndYear();
+	}
+	else if (dep != "" && annee == "tous" && dip != ""){
+		getAllByDepAndDip();
+	}
+	else if (dep == "" && annee != "tous" && dip != ""){
+		getAllByDipAndYear();
 	}
 	else {
-	getAllByNameAndYear();
+	getAllByDepDipAnnee();
 	}
 }
-function getAllByNameAndYear(){
+function getAllByDepDipAnnee(){
 	loadXMLFile();
     document.write("<table  class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
     var x=xmlDoc.getElementsByTagName("ETU");
     for (i=0;i<x.length;i++){
 		if(x[i].parentNode.getAttribute("Id") == annee){
-			if(x[i].getAttribute("LastName").toLowerCase() == name.toLowerCase()){
+			if(encodeURIComponent(x[i].parentNode.parentNode.getAttribute("Name").toLowerCase().trim()) == dip.toLowerCase()){
+				if(x[i].parentNode.parentNode.parentNode.getAttribute("Name").toLowerCase() == dep.toLowerCase()){
+					document.write("<tr>");
+					document.write("<td>");
+					document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
+					document.write("</td>");
+					document.write("<td>");
+					document.write(x[i].parentNode.parentNode.getAttribute('Name'));
+					document.write("</td>");
+					document.write("<td>");
+					document.write(x[i].parentNode.getAttribute('Id'));
+					document.write("</td>");
+					document.write("<td>");
+					document.write("<b><a onclick= "+"actualisation("+x[i].getAttribute('Num')+") >"+x[i].getAttribute('LastName')+" "+x[i].getAttribute('Name')+"</a></b>");
+					document.write("</td>");
+					document.write("<td>");
+					document.write(x[i].getAttribute('Num'));
+					document.write("</td>");
+					document.write("</tr>");
+				}
+			}
+		}
+	}
+    document.write("</table>");
+}
+function getAllByDepAndYear(){
+	loadXMLFile();
+    document.write("<table  class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
+    var x=xmlDoc.getElementsByTagName("ETU");
+    for (i=0;i<x.length;i++){
+		if(x[i].parentNode.getAttribute("Id") == annee){
+			if(x[i].parentNode.parentNode.parentNode.getAttribute("Name").toLowerCase() == dep.toLowerCase()){
 				document.write("<tr>");
 				document.write("<td>");
 				document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
@@ -189,12 +168,97 @@ function getAllByNameAndYear(){
 	}
     document.write("</table>");
 }
-function getAllByName(){
+function getAllByDipAndYear(){
+	loadXMLFile();
+    document.write("<table  class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
+    var x=xmlDoc.getElementsByTagName("ETU");
+    for (i=0;i<x.length;i++){
+		if(x[i].parentNode.getAttribute("Id") == annee){
+			if(encodeURIComponent(x[i].parentNode.parentNode.getAttribute("Name").toLowerCase().trim()) == dip.toLowerCase()){
+				document.write("<tr>");
+				document.write("<td>");
+				document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].parentNode.parentNode.getAttribute('Name'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].parentNode.getAttribute('Id'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write("<b><a onclick= "+"actualisation("+x[i].getAttribute('Num')+") >"+x[i].getAttribute('LastName')+" "+x[i].getAttribute('Name')+"</a></b>");
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].getAttribute('Num'));
+				document.write("</td>");
+				document.write("</tr>");
+			}
+		}
+	}
+    document.write("</table>");
+}
+function getAllByDepAndDip(){
+	loadXMLFile();
+    document.write("<table  class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
+    var x=xmlDoc.getElementsByTagName("ETU");
+    for (i=0;i<x.length;i++){
+		if(x[i].parentNode.parentNode.parentNode.getAttribute("Name").toLowerCase() == dep.toLowerCase()){
+			if(encodeURIComponent(x[i].parentNode.parentNode.getAttribute("Name").toLowerCase().trim()) == dip.toLowerCase()){
+				document.write("<tr>");
+				document.write("<td>");
+				document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].parentNode.parentNode.getAttribute('Name'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].parentNode.getAttribute('Id'));
+				document.write("</td>");
+				document.write("<td>");
+				document.write("<b><a onclick= "+"actualisation("+x[i].getAttribute('Num')+") >"+x[i].getAttribute('LastName')+" "+x[i].getAttribute('Name')+"</a></b>");
+				document.write("</td>");
+				document.write("<td>");
+				document.write(x[i].getAttribute('Num'));
+				document.write("</td>");
+				document.write("</tr>");
+			}
+		}
+	}
+    document.write("</table>");
+}
+function getAllByDep(){
     loadXMLFile();
     document.write("<table class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
     var x=xmlDoc.getElementsByTagName("ETU");
     for (i=0;i<x.length;i++){
-		if(x[i].getAttribute("LastName").toLowerCase() == name.toLowerCase()){
+		if(x[i].parentNode.parentNode.parentNode.getAttribute("Name").toLowerCase() == dep.toLowerCase()){
+			document.write("<tr>");
+			document.write("<td>");
+			document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
+			document.write("</td>");
+			document.write("<td>");
+			document.write(x[i].parentNode.parentNode.getAttribute('Name'));
+			document.write("</td>");
+			document.write("<td>");
+			document.write(x[i].parentNode.getAttribute('Id'));
+			document.write("</td>");
+			document.write("<td>");
+			document.write("<b><a onclick= "+"actualisation("+x[i].getAttribute('Num')+") >"+x[i].getAttribute('LastName')+" "+x[i].getAttribute('Name')+"</a></b>");
+			document.write("</td>");
+			document.write("<td>");
+			document.write(x[i].getAttribute('Num'));
+			document.write("</td>");
+			document.write("</tr>");
+		}
+	}
+    document.write("</table>");
+}
+function getAllByDip(){
+    loadXMLFile();
+    document.write("<table class='tabcenter'><tr><th>DEP</th><th>DIPLOME</th><th>PROMO</th><th>NOM</th><th>NUM</th></tr>");
+    var x=xmlDoc.getElementsByTagName("ETU");
+    for (i=0;i<x.length;i++){
+		if(encodeURIComponent(x[i].parentNode.parentNode.getAttribute("Name").toLowerCase().trim()) == dip.toLowerCase()){
 			document.write("<tr>");
 			document.write("<td>");
 			document.write(x[i].parentNode.parentNode.parentNode.getAttribute('Name'));
@@ -269,7 +333,7 @@ function getAllEmpty(){
         document.write("</tr>");
     }
     document.write("</table>");
-0}
+}
 
 function getAllbyNumBis(){
 	isAfter = false;
